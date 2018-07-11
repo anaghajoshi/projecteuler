d949e686914fad6efdb5fd0d0e5a9df6afb3b65c
Anagha Joshi <anagha.elex@gmail.com>
2018-07-11 11:39:14 -0700

    Problem 1 modified

--- Problem_1.py
+++ Problem_1.py
@@ -1,13 +1,29 @@
-#Multiples of 3 and 5
+
+# 
+# Project Euler problem 1
+# Copyright (c) Project anaghajoshi. All rights reserved.
+#  
+# https://github.com/anaghajoshi/projecteuler
+# 
+
 """
+Multiples of 3 and 5
+
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
 Find the sum of all the multiples of 3 or 5 below 1000.
+
 """
 
-sum = 0
-for i in range(0,10):
-	if (i%3 == 0) or (i%5 ==0):
-		sum += i
+# 
+def add_multiples(num):
+	sum = 0
+	for i in range(0,num):
+		if (i%3 == 0) or (i%5 ==0):
+			sum += i
+	return sum
 
-print(sum)
+
+if __name__ == "__main__":
+	num = input("enter the limiting number: ")       
+	print(add_multiples(int(num)))
+
