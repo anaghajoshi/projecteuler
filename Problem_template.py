981da3f33eddd8db5b61045fa445849ffddb168a
Anagha Joshi <anagha.elex@gmail.com>
2018-07-11 11:59:45 -0700

    add template and problem 2 solution

---
+++ Problem_template.py
@@ -0,0 +1,21 @@
+
+# 
+# Project Euler problem 
+# Copyright (c) Project anaghajoshi. All rights reserved.
+#  
+# https://github.com/anaghajoshi/projecteuler
+# 
+
+"""
+
+
+"""
+
+# 
+def add_multiples():
+	#
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(add_multiples())
